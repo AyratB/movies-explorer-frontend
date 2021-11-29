ef99792c41736162be75df67760832ec065feb2b
Ayrat Burnashev <airatbyr@ya.ru>
2021-11-29 17:57:59 +0300

    fix: edit backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,11 @@
 export const SIZE_NEED_BREAD_CRUMBS = 768;
 
-// export const BASE_MAIN_URL = "https://api.aburnashev-movies.nomoredomains.club";
+export const BASE_MAIN_URL = "https://api.aburnashev-movies.nomoredomains.club";
 // обращение к нашему api на локальной машине
-export const BASE_MAIN_URL = "http://localhost:3006";
+// export const BASE_MAIN_URL = "http://localhost:3006";
 
 export const BASE_MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 export const BASE_MOVIES_IMAGE_URL = "https://api.nomoreparties.co";
-
 
 export const savedFakeMovieData = [
     {
