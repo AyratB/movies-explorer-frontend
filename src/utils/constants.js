75b3a6f3c0b5b653d1343e7b63370df34fa7d667
Ayrat Burnashev <airatbyr@ya.ru>
2021-12-21 22:39:33 +0300

    feat: BASE_MAIN_URL change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const SIZE_NEED_BREAD_CRUMBS = 768;
 
-//export const BASE_MAIN_URL = "https://api.aburnashev-movies.nomoredomains.club";
+export const BASE_MAIN_URL = "https://api.aburnashev-movies.nomoredomains.club";
 
-export const BASE_MAIN_URL = "http://localhost:3006";
+//export const BASE_MAIN_URL = "http://localhost:3006";
 
 export const BASE_MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 export const BASE_MOVIES_IMAGE_URL = "https://api.nomoreparties.co";
